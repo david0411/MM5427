1640c2665b851d36313f0d44d38483e70082574f
david0411 <tlyeyd@gmail.com>
2024-03-17 00:32:13 +0800

    bag of words

--- Project/main.py
+++ Project/main.py
@@ -4,14 +4,16 @@ import pandas as pd
 import numpy as np
 from nltk.corpus import stopwords
 
+
+word_count_list =[]
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 
 x = pd.read_csv('AnnualReports1618.csv', header=0)
 x_2016 = x[x['filed_date'] < 20170000]
 
-# for i in range(x_2016.shape[0]):
-for i in range(20):
+for i in range(x_2016.shape[0]):
+# for i in range(20):
     content = str(x_2016['item7'][i]).lower()
     if content != 'nan':
         words = set(re.split('[ (),.]', content))
@@ -22,7 +24,7 @@ for i in range(20):
                 discard_set.add(num)
             if '$' in item or '%' in item or item == '':
                 discard_set.add(item)
-        print(discard_set)
         for item in discard_set:
             words_swr.discard(item)
-        print(words_swr)
+        word_count_list.append([x_2016['company_name'][i], len(words_swr)])
+pd.DataFrame(word_count_list, columns=['Company Name', 'Word Count']).to_excel('word_count.xlsx', index=False)
